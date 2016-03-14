5307698f48f0e768f91a0b6b004950afe5d5a087
Tomasz Gabrysiak <tomsonwch@gmail.com>
2016-03-14 14:25:28 +0100

    Change fields type in model

--- backend/restapp/books/models.py
+++ backend/restapp/books/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.TextField()
-    description = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=30)
+    description = models.TextField()
 
     def __unicode__(self):
         return "{} {}".format(self.first_name, self.last_name)
 
 class Book(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
