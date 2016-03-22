718368e80a9839059d30548f7c582ae407253cac
Tomasz Gabrysiak <tomsonwch@gmail.com>
2016-03-22 09:07:27 +0100

    Create simple frontend app

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,3 +3,10 @@
  */
 
 
+var app = angular.module("myRestApp", []);
+
+app.controller("myRestAppCtrl", function($scope) {
+  $scope.books = ["Book 1", "Book 2"];
+  $scope.authors = ["Author 1", "Author 2", "Author 3", "Author 4"];
+});
+
