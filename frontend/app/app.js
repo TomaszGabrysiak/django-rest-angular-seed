3b6cb211426afbd098ba79238ff745b7912457e0
Tomasz Gabrysiak <tomsonwch@gmail.com>
2016-03-24 11:42:34 +0100

    Rename controller

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -5,8 +5,9 @@
 
 var app = angular.module("myRestApp", []);
 
-app.controller("myRestAppCtrl", function($scope) {
+app.controller("MyRestAppCtrl", function($scope) {
   $scope.books = ["Book 1", "Book 2"];
   $scope.authors = ["Author 1", "Author 2", "Author 3", "Author 4"];
 });
+
 
